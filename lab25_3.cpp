8cacd3e034737a86cce80024a7fc4686d68f940f
Piicharpa <neuah2515@gmail.com>
2023-03-10 17:20:17 +0700

    complete

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-//Write function count() here.
+
+
+int count(int data[],int k){
+    set<int> N;
+    
+        for(int j = 0;j < k; j++){
+        N.insert(data[j]);
+        }
+    
+    return N.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
